464fc56a622dff62fbb9e36f8a61dc3bf361a105
Laiba Choudhry <laiba@hotmail.nl>
2023-05-25 19:29:30 +0200

    ugh

--- enquete.js
+++ enquete.js
@@ -1,7 +1,4 @@
 const inputs = document.querySelectorAll("input[type='text'], input[type='email']");
-// const voornaamInput = document.getElementById("voornaam");
-// const achternaamInput = document.getElementById("achternaam");
-// const studentennummerInput = document.getElementById("studentennummer");
 const inputRadios = document.querySelectorAll("input[type='radio']");
 const question_1 = document.getElementById("vraag1");
 const question_2 = document.getElementById("vraag2");
